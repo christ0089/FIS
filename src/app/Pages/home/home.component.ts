11d1d12fd7213fa5e7fce4e5da1c1dd40982c32e
ernestognw <ernestognw@gmail.com>
2019-05-01 13:25:00 -0500

    ahi le va joven xd

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../Services/product_service';
 import { Product } from '../../Class /Product';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +8,14 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  products:Product[] = [];
+  products:any = [];
 
   constructor(
     public productService: ProductService
   ) {
-
+    productService.getProducts().subscribe(products => {
+      this.products = products;
+    });
   }
 
   ngOnInit() {
